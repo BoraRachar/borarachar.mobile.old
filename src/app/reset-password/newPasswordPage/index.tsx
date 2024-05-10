ecba6ae8f528041681a5919fbb71fd89167ba4ea
Junior Alves <jrnalves@gmail.com>
2024-05-10 15:44:13 -0300

    feat: Adionar scroll para o fim da pagina quando houver foco no input de adcionar senha

--- src/app/reset-password/newPasswordPage/index.tsx
+++ src/app/reset-password/newPasswordPage/index.tsx
@@ -1,5 +1,6 @@
 import {
   Pressable,
+  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
@@ -12,7 +13,7 @@ import { Ionicons } from '@expo/vector-icons'
 import { theme } from '@/src/theme'
 
 import { styles } from './styles'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 type FormData = {
   newPassword: 'string'
@@ -36,6 +37,8 @@ const schema = yup.object().shape({
 })
 
 export default function NewPasswordPage() {
+  const [hidden, setHidden] = useState(true)
+  const [hidden2, setHidden2] = useState(true)
   const {
     register,
     setValue,
@@ -44,93 +47,102 @@ export default function NewPasswordPage() {
   } = useForm<FormData>({
     resolver: yupResolver(schema),
   })
-  const [hidden, setHidden] = useState(true)
-  const [hidden2, setHidden2] = useState(true)
+
+  const scrollViewRef = useRef()
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data)
   }
 
+  const handleFocus = () => {
+    scrollViewRef.current?.scrollToEnd({ animated: true })
+  }
+
   return (
     <>
       {/* main */}
-      <View>
-        <Text style={styles.subTitle}>É fácil redefinir sua senha.</Text>
-        <Text style={styles.description}>
-          Atualize sua senha para manter sua conta segura!
-        </Text>
-      </View>
-
-      <View style={{ flex: 1, marginTop: 24 }}>
+      <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false}>
         <View>
-          <View>
-            <Text style={styles.label}>Nova senha</Text>
-            <View style={styles.inputTextWrapper}>
-              <TextInput
-                style={styles.inputText}
-                autoFocus={true}
-                secureTextEntry={hidden}
-                maxLength={20}
-                {...register('newPassword')}
-                onChangeText={(text) => setValue('newPassword', text)}
-              />
-              <Pressable onPress={() => setHidden(!hidden)}>
-                <Ionicons
-                  name={hidden === true ? 'eye-outline' : 'eye-off-outline'}
-                  color={theme.colors.primary}
-                  size={25}
-                />
-              </Pressable>
-            </View>
-            {errors && (
-              <Text style={styles.errorMessage}>
-                {errors.newPassword?.message}
-              </Text>
-            )}
-          </View>
+          <Text style={styles.subTitle}>É fácil redefinir sua senha.</Text>
+          <Text style={styles.description}>
+            Atualize sua senha para manter sua conta segura!
+          </Text>
+        </View>
 
+        <View style={{ flex: 1, marginTop: 24 }}>
           <View>
-            <Text style={styles.label}>Confirmar nova senha</Text>
-            <View style={styles.inputTextWrapper}>
-              <TextInput
-                style={styles.inputText}
-                autoFocus={true}
-                secureTextEntry={hidden2}
-                maxLength={20}
-                {...register('newPasswordConfirm')}
-                onChangeText={(text) => setValue('newPasswordConfirm', text)}
-              />
-              <Pressable onPress={() => setHidden2(!hidden2)}>
-                <Ionicons
-                  name={hidden2 === true ? 'eye-outline' : 'eye-off-outline'}
-                  color={theme.colors.primary}
-                  size={25}
+            {/* InputText 1 */}
+            <View>
+              <Text style={styles.label}>Nova senha</Text>
+              <View style={styles.inputTextWrapper}>
+                <TextInput
+                  style={styles.inputText}
+                  autoFocus={false}
+                  secureTextEntry={hidden}
+                  maxLength={20}
+                  {...register('newPassword')}
+                  onChangeText={(text) => setValue('newPassword', text)}
+                  onFocus={() => handleFocus()}
                 />
-              </Pressable>
+                <Pressable onPress={() => setHidden(!hidden)}>
+                  <Ionicons
+                    name={hidden === true ? 'eye-outline' : 'eye-off-outline'}
+                    color={theme.colors.primary}
+                    size={25}
+                  />
+                </Pressable>
+              </View>
+              {errors && (
+                <Text style={styles.errorMessage}>
+                  {errors.newPassword?.message}
+                </Text>
+              )}
             </View>
-            {errors && (
-              <Text style={styles.errorMessage}>
-                {errors.newPasswordConfirm?.message}
-              </Text>
-            )}
-          </View>
 
-          <View>
-            <Text style={styles.descriptionPassword}>
-              Deve conter no mínimo 8 caracteres sendo:
-            </Text>
-            <Text
-              style={[styles.descriptionPassword, { marginLeft: 6 }]}
-            >{`\u2022 1 maiúsculo`}</Text>
-            <Text
-              style={[styles.descriptionPassword, { marginLeft: 6 }]}
-            >{`\u2022 1 minúsculo`}</Text>
-            <Text style={[styles.descriptionPassword, { marginLeft: 6 }]}>
-              {`\u2022 1 especial (!, @, #, $, %, &, *, - ...)`}
-            </Text>
+            {/* InputText2 */}
+            <View>
+              <Text style={styles.label}>Confirmar nova senha</Text>
+              <View style={styles.inputTextWrapper}>
+                <TextInput
+                  style={styles.inputText}
+                  secureTextEntry={hidden2}
+                  maxLength={20}
+                  {...register('newPasswordConfirm')}
+                  onChangeText={(text) => setValue('newPasswordConfirm', text)}
+                  onFocus={() => handleFocus()}
+                />
+                <Pressable onPress={() => setHidden2(!hidden2)}>
+                  <Ionicons
+                    name={hidden2 === true ? 'eye-outline' : 'eye-off-outline'}
+                    color={theme.colors.primary}
+                    size={25}
+                  />
+                </Pressable>
+              </View>
+              {errors && (
+                <Text style={styles.errorMessage}>
+                  {errors.newPasswordConfirm?.message}
+                </Text>
+              )}
+            </View>
+
+            <View>
+              <Text style={styles.descriptionPassword}>
+                Deve conter no mínimo 8 caracteres sendo:
+              </Text>
+              <Text
+                style={[styles.descriptionPassword, { marginLeft: 6 }]}
+              >{`\u2022 1 maiúsculo`}</Text>
+              <Text
+                style={[styles.descriptionPassword, { marginLeft: 6 }]}
+              >{`\u2022 1 minúsculo`}</Text>
+              <Text style={[styles.descriptionPassword, { marginLeft: 6 }]}>
+                {`\u2022 1 especial (!, @, #, $, %, &, *, - ...)`}
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
+      </ScrollView>
 
       {/* footer */}
       <View style={{ paddingBottom: 24 }}>
