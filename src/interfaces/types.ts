a8db1ec14a014bb3f843e76bef13047763be214d
Alysson Chiarelly <alyssonclr@gmail.com>
2024-05-16 19:01:09 -0300

    feat: foi adicionado a chamada a api que envia o código para o email

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,4 +1,13 @@
-export interface ErrorResponse {
+interface ErrorResponse {
   statusCode: number
   errors: { userMessage: string }[]
 }
+
+interface ResetPasswordData {
+  email: string
+  code: string
+  novaSenha: string
+  confirmacaoSenha: string
+}
+
+export { ErrorResponse, ResetPasswordData }
