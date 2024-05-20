2ed7e01c70becc1e36a9d23124af1f577f96f099
Alysson Chiarelly <alyssonclr@gmail.com>
2024-05-20 12:31:54 -0300

    feat: foi removido o log e adicionado uma permissão de teste

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL
-console.log('API_BASE_URL:', API_BASE_URL)
 
 export const axiosClient = axios.create({
   baseURL: API_BASE_URL,
